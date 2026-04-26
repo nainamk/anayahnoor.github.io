ce4e7cd909ce190ffa3c8f3be1fa65cbfcd97c5e
nainamk <naina.mk0505@gmail.com>
2026-04-25 21:36:10 -0400

    Create service-worker.js

---
+++ pwa/service-worker.js
@@ -0,0 +1,21 @@
+const CACHE = "pwa-cache-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./data.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
